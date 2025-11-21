01b458e57cf66665b5e1730d43d8edb38218f85f
Shyam261510 <shyam261510@gmail.com>
2025-11-21 15:40:19 +0530

    change prisma version

--- src/libs/prisma.ts
+++ src/libs/prisma.ts
@@ -1,9 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
-  primsa: PrismaClient | undefined;
+  prisma: PrismaClient | undefined;
 };
 
-export const prisma = globalForPrisma.primsa ?? new PrismaClient();
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.primsa = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
